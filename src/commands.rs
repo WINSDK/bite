218a3d65d597949eb30ad4cbf5a523941b846e63
Nicolas Mazzon <nicolasmazzon549@gmail.com>
2023-07-02 12:15:28 +0200

    CD command with homedir expansion.

--- src/commands.rs
+++ src/commands.rs
@@ -1,6 +1,8 @@
+use std::path::Path;
+
 use crate::gui::RenderContext;
 
-const CMDS: &[&str] = &["open", "pwd"];
+const CMDS: &[&str] = &["open", "pwd", "cd"];
 
 fn possible_command(unknown: &str) -> Option<&str> {
     let mut distance = u32::MAX;
@@ -17,6 +19,41 @@ fn possible_command(unknown: &str) -> Option<&str> {
     (distance <= 2).then_some(best_guess)
 }
 
+fn print_cwd(ctx: &mut RenderContext) {
+    match std::env::current_dir() {
+        Ok(path) => ctx
+            .terminal_prompt
+            .push_str(&format!("Working directory {}.\n", path.display())),
+        Err(err) => ctx.terminal_prompt.push_str(&format!("Failed to print pwd: '{err}'\n")),
+    }
+}
+
+fn expand_homedir<P: AsRef<Path>>(path: P) -> std::path::PathBuf {
+    let path = path.as_ref();
+
+    if !path.starts_with("~") {
+        return path.to_path_buf();
+    }
+
+    let mut home_dir = match dirs::home_dir() {
+        Some(dir) => dir,
+        None => return path.to_path_buf(),
+    };
+
+    if path == Path::new("~") {
+        return home_dir;
+    }
+
+    if home_dir == Path::new("/") {
+        // Corner case: `home_dir` root directory;
+        // don't prepend extra `/`, just drop the tilde.
+        path.strip_prefix("~").unwrap().to_path_buf()
+    } else {
+        home_dir.push(path.strip_prefix("~/").unwrap());
+        home_dir
+    }
+}
+
 pub fn process_commands(ctx: &mut RenderContext, commands: &[String]) {
     for cmd in commands {
         ctx.terminal_prompt.push_str(&format!("(bite) {cmd}\n"));
@@ -30,23 +67,37 @@ pub fn process_commands(ctx: &mut RenderContext, commands: &[String]) {
         let arg = delimited.next();
 
         if cmd == "open" {
-            if let Some(path) = arg {
-                ctx.start_disassembling(path.to_string());
-                ctx.terminal_prompt.push_str(&format!("Binary '{path}' was opened.\n"));
+            if let Some(unexpanded) = arg {
+                let path = expand_homedir(unexpanded);
+
+                ctx.start_disassembling(path);
+                ctx.terminal_prompt.push_str(&format!("Binary '{unexpanded}' was opened.\n"));
                 continue;
             }
 
             ctx.terminal_prompt.push_str(&format!("Command 'open' requires a path.\n"));
-            continue
+            continue;
+        }
+
+        if cmd == "cd" {
+            if let Some(path) = arg {
+                let path = expand_homedir(path);
+
+                if let Err(err) = std::env::set_current_dir(path) {
+                    ctx.terminal_prompt
+                        .push_str(&format!("Failed to change directory: '{err}'.\n"));
+                    continue;
+                }
+                print_cwd(ctx);
+                continue;
+            }
+
+            ctx.terminal_prompt.push_str(&format!("Command 'cd' requires a path.\n"));
+            continue;
         }
 
         if cmd == "pwd" {
-            match std::env::current_dir() {
-                Ok(path) => {
-                    ctx.terminal_prompt.push_str(&format!("Working dir {}.\n", path.display()))
-                }
-                Err(err) => ctx.terminal_prompt.push_str(&format!("{err:?}\n")),
-            }
+            print_cwd(ctx);
             continue;
         }
 
