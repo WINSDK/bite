df85cb4af121a3cc9be0ef8f754b3ea2e2943010
Nicolas Mazzon <nicolasmazzon549@gmail.com>
2023-07-04 20:03:48 +0200

    MacOS module.

--- debugger/src/lib.rs
+++ debugger/src/lib.rs
@@ -1,11 +1,17 @@
 #[cfg(target_os = "linux")]
 mod linux;
 
+#[cfg(target_os = "macos")]
+mod macos;
+
 #[cfg(target_os = "windows")]
 mod windows;
 
 #[cfg(target_os = "linux")]
 use linux::*;
+
+#[cfg(target_os = "macos")]
+use macos::*;
 
 #[cfg(target_os = "windows")]
 use windows::*;
