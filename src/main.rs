48c662637be9860d39a030002d52484d268ca045
Nicolas Mazzon <nicolasmazzon549@gmail.com>
2023-07-15 12:45:06 +0200

    Ability to run gui as root on wayland.

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,41 @@ use std::fs;
 
 static ARGS: Lazy<args::Cli> = Lazy::new(args::Cli::parse);
 
+fn get_first_direntry<P: AsRef<std::path::Path>>(path: P) -> Option<std::ffi::OsString> {
+    if let Ok(mut dir) = std::fs::read_dir(path) {
+        if let Some(Ok(entry)) = dir.next() {
+            return Some(entry.file_name());
+        }
+    }
+
+    None
+}
+
+fn set_wayland_env() {
+    // if the necessary wayland env variables aren't set, try to guess them
+    if std::env::var("XDG_RUNTIME_DIR").is_err() || std::env::var("WAYLAND_DISPLAY").is_err() {
+        let user = match get_first_direntry("/run/user") {
+            Some(user) => user,
+            None => {
+                warning!("Failed to guess wayland environmental variables.");
+                return;
+            }
+        };
+
+        let mut path = std::path::PathBuf::from("/run/user/");
+        path.push(user);
+        path.push("wayland-1");
+
+        std::env::set_var("WAYLAND_DISPLAY", path);
+        std::env::set_var("XDG_RUNTIME_DIR", "/");
+    }
+}
+
 fn main() {
+    if unsafe { libc::getuid() } == 0 {
+        set_wayland_env();
+    }
+
     if ARGS.disassemble {
         return gui::init().unwrap();
     }
